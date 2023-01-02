7b87de9f7d75676b817a9454ea8f99d360d82906
badr ghanbi <bghanbi50@gmail.com>
2023-01-02 13:32:05 +0100

    validate input with formik

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,78 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import * as Yup from 'yup'
 import './App.css'
+import { useFormik } from 'formik';
 
-import { Formik } from 'formik';
+const App = () => {
 
-const App = () => (
-  <div>
-    <h1>Anywhere in your app!</h1>
-    <Formik
-      initialValues={{ email: '', password: '' }}
-      validate={values => {
-        const errors = {};
-        if (!values.email) {
-          errors.email = 'Required';
-        } else if (
-          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-        ) {
-          errors.email = 'Invalid email address';
-        }
-        return errors;
-      }}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
-          setSubmitting(false);
-        }, 400);
-      }}
-    >
-      {({
-        values,
-        errors,
-        touched,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        isSubmitting,
-        /* and other goodies */
-      }) => (
-        <form onSubmit={handleSubmit}>
-          <input
-            type="email"
-            name="email"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.email}
-          />
-          {errors.email && touched.email && errors.email}
-          <input
-            type="password"
-            name="password"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.password}
-          />
-          {errors.password && touched.password && errors.password}
-          <button type="submit" disabled={isSubmitting}>
-            Submit
-          </button>
-        </form>
-      )}
-    </Formik>
-  </div>
-);
+  const formik=useFormik({
+    initialValues :{
+      name:'',
+      email:'',
+      password:''
+
+    },
+
+     validationSchema:Yup.object({
+       name:Yup.string().max(15,"Must be 15 character or less")
+       .required(`can't be empty`),
+       email:Yup.string().email('Invalid email').required(`can't be empty`),
+       password:Yup.string().min(8,'to shoort').required(`can't be empty`)
+     }),
+
+    onSubmit:(values)=>{
+      console.log('submited')
+    }
+  });
+  
+  console.log(formik.errors)
+   return(
+    <form onSubmit={formik.handleSubmit}>
+    <div className='input-field'>
+      <input
+       type='text'
+       name='name'
+       placeholder='enter name'
+       value={formik.values.name}
+      onChange={formik.handleChange}
+       onBlur={formik.handleBlur}
+       className={`${formik.errors.name ? 'invalidInput':'validInput'}`}
+
+       />
+       { formik.touched.name &&  formik.errors.name && <p className='error'>{formik.errors.name}</p>}
+    </div>
+
+    <div className='input-field'>
+      <input
+       type='email'
+       name='email'
+       placeholder='enter email'
+       value={formik.values.email}
+     onBlur={formik.handleBlur}
+       onChange={formik.handleChange}
+
+       />
+       { formik.touched.email && formik.errors.email && <p className='error'>{formik.errors.email}</p>}
+
+    </div>
+
+    <div className='input-field'>
+      <input
+       type='password'
+       name='password'
+       onChange={formik.handleChange}
+       placeholder='enter Password' 
+        onBlur={formik.handleBlur}
+       value={formik.values.password}
+       />
+              {  formik.touched.password && formik.errors.password && <p className='error'>{formik.errors.password}</p>}
+
+    </div>
+
+    <button type='submit' >Submit</button>
+    </form>
+   )
+ };
 
 export default App;
